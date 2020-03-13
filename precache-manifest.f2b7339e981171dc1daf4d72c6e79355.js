a75e9b7a6e0dd574e4cb4b2fc4f491bfb0e700d6
tsaristbomba <andrexerdna@gmail.com>
2020-03-13 13:35:25 -0300

    Updates

---
+++ precache-manifest.f2b7339e981171dc1daf4d72c6e79355.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5b6dcd4e29ec360d0f88c858bc6851",
+    "url": "/js-calculator/index.html"
+  },
+  {
+    "revision": "36c49b7d4fa42f33acbd",
+    "url": "/js-calculator/static/css/main.da6da9fa.chunk.css"
+  },
+  {
+    "revision": "7ceb3f22bc161a043316",
+    "url": "/js-calculator/static/js/2.ca1d9be5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/js-calculator/static/js/2.ca1d9be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c49b7d4fa42f33acbd",
+    "url": "/js-calculator/static/js/main.c06f7860.chunk.js"
+  },
+  {
+    "revision": "a2d25a265051b2b1db49",
+    "url": "/js-calculator/static/js/runtime-main.3a9fc260.js"
+  }
+]);
